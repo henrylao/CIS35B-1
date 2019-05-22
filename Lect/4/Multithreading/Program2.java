a0e4d037dbc1596593cc1e086378851369ad52cc
tt577 <yangtq2014@gmail.com>
2019-05-21 23:47:22 -0700

    lab4

--- Lect/4/Multithreading/Program2.java
+++ Lect/4/Multithreading/Program2.java
@@ -1,11 +1,11 @@
 //Many threads of the same class and each has its own name.
-public class simpleThread extends Thread
+public class Program2 extends Thread
 {
 	private int countDown = 5;
 	private int threadNumber;
 	private static int threadCount = 0;
 	
-	public simpleThread()
+	public Program2()
 	{
 		threadNumber = ++threadCount;
 		System.out.println("Making "+ threadNumber);
@@ -24,7 +24,7 @@ public class simpleThread extends Thread
 	public static void main (String[] args)
 	{
 		for(int I = 0; I < 5; I++)
-			new simpleThread().start();
+			new Program2().start();
 		System.out.println("All threads started");
 	}
 }
