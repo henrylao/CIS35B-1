ffa5f9bd020960785740956010209290b6349a3d
tt577 <yangtq2014@gmail.com>
2019-06-22 12:59:35 -0700

    update

--- Lab6/Lab6Client/src/adapter/Debuggable.java
+++
@@ -1,5 +0,0 @@
-package adapter;
-
-public interface Debuggable {
-	final boolean DEBUG = true;
-}
--- Lab6/Lab6Server/src/client/AutoClient.java
+++
@@ -1,5 +0,0 @@
-package client;
-
-public interface AutoClient {
-	public void client(String host, int port);
-}
